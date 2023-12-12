d15ff81d114f11c218bcd7b92a9f2a7d4975a549
nitter1 <danielroger2001@gmail.com>
2023-12-11 23:01:56 -0300

    newPlayer

--- src/app/player/page.js
+++ src/app/player/page.js
@@ -40,7 +40,7 @@ export default function Player() {
                 </section>
 
                 <section className={styles.containerDir}>
-                <iframe src="https://zeno.fm/player/radio-divinal-digital" width="768" height="500" frameborder="0" scrolling="no"></iframe><a href="https://zeno.fm/" target="_blank" style={{ display: 'block', fontSize: '0.9em', lineHeight: '10px' }}></a>
+                <iframe src="https://player.srvsh.com.br/player-web/8024" frameborder="0" width="100%" height="465"></iframe>
                 </section>
             </main>
 
